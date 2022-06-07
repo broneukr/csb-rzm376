07edf09c14129edc2e38062045ef6a6907b106a7
Code Sandbox <hello@codesandbox.io>
2022-06-07 16:51:35 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.107355597bb89e54d9658506d70df83d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41de86c1f97f3861f61041f42af92cd5",
+    "url": "https://broneukr.github.io/csb-rzm376/index.html"
+  },
+  {
+    "revision": "9f890df1f16529c567c5",
+    "url": "https://broneukr.github.io/csb-rzm376/static/js/2.988d9159.chunk.js"
+  },
+  {
+    "revision": "6aca111766df46380727",
+    "url": "https://broneukr.github.io/csb-rzm376/static/js/main.d0f70e33.chunk.js"
+  },
+  {
+    "revision": "184c684b958b5f41fbb4",
+    "url": "https://broneukr.github.io/csb-rzm376/static/js/runtime~main.3c28c4d9.js"
+  }
+]);
